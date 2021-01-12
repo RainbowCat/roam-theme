59796bd0c616765a3dfe710168b10fbdff44969a
rainbowCat <jazziblue@gmail.com>
2021-01-12 14:54:18 -0800

    styling topbar

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-// hide all unecessary 
-$(".parent").each(function()
-{
-    if($(this).children(".child").length == 0)
-    {
-        $(this).hide();
-    }
-});
+// // hide all unecessary
+// $(".parent").each(function () {
+//   if ($(this).children(".child").length == 0) {
+//     $(this).hide();
+//   }
+// });
 
+// var divLine = document.createElement("hr");
+// var shortcutMenu = document.getElementByClass("starred-pages-wrapper"); // Get the <ul> element to insert a new node
+// document.body.insertBefore(divLine, shotcutMenu);
+// divLine.sytle = "border: 1px solid white;";
